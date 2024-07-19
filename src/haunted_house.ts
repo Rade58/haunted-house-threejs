3114afdf0e07368c688795d8e242b3837ae4bc44
Rade58 <bajic.rade2@gmail.com>
2024-07-20 01:53:16 +0200

    initial setup

---
+++ src/haunted_house.ts
@@ -0,0 +1,251 @@
+// 1 unit will represent 1 meter (we decided)
+
+import * as THREE from "three";
+import { /* FontLoader, */ OrbitControls } from "three/examples/jsm/Addons.js";
+// import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+// import gsap from "gsap";
+import GUI from "lil-gui";
+
+/**
+ * @description Debug UI - lil-ui
+ */
+const gui = new GUI({
+  width: 340,
+  title: "Debugging",
+  closeFolders: false,
+});
+
+gui.hide();
+/* 
+const debugObject = {
+  color: "#90315f",
+  subdivisions: 1,
+  //
+  // spin: () => {},
+}; */
+
+const sizes = {
+  // width: 800,
+  width: window.innerWidth,
+  // height: 600,
+  height: window.innerHeight,
+};
+
+const canvas: HTMLCanvasElement | null = document.querySelector("canvas.webgl");
+
+if (canvas) {
+  // -------------------------------------------------------
+
+  const scene = new THREE.Scene();
+
+  // loading the shadow texture
+
+  const textureLoader = new THREE.TextureLoader();
+
+  // ------  LIGHTS
+  // --------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------------------
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+
+  directionalLight.position.set(2, 2, -1);
+
+  scene.add(directionalLight);
+
+  const directionalLightHelper = new THREE.DirectionalLightHelper(
+    directionalLight,
+    0.2,
+    "purple"
+  );
+  // scene.add(directionalLightHelper);
+
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // ------ MATERIALS ------
+
+  // --------------------------------------------------------------
+  // ------ GEOMETRIES ------
+  const sphereGeo = new THREE.SphereGeometry(1, 32, 32);
+  const planeGeo = new THREE.PlaneGeometry(20, 20);
+
+  // --------------------------------------------------------------
+  // ------ MESHES ------------------------------------------------
+  const sphere = new THREE.Mesh(
+    sphereGeo,
+    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+  );
+  sphere.position.y = 2;
+  scene.add(sphere);
+
+  const floor = new THREE.Mesh(
+    planeGeo,
+    new THREE.MeshStandardMaterial({ color: "#a9c388" })
+  );
+  floor.rotation.x = -Math.PI * 0.5; // this is -90deg
+  // floor.position.y = -0.65;
+
+  scene.add(floor);
+
+  //  ---------------------- SHADOWS RELATED ----------------------
+  // --------------------------------------------------------------
+  // --------------------------------------------------------------
+  // --------------------------------------------------------------
+
+  // --------------------------------------------------------
+
+  // --------------------------------------------------------------
+  // --------------------------------------------------------------
+  // --------------------------------------------------------------
+  // --------------------------------------------------------------
+
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  //  gui
+  const lightsFolder = gui.addFolder("Lights");
+
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+  // -----------------------------------------------------------------------
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+
+    0.1,
+    100
+  );
+
+  camera.position.z = 3;
+  camera.position.x = 1;
+  camera.position.y = 1;
+  scene.add(camera);
+
+  const axHelp = new THREE.AxesHelper(4);
+  axHelp.setColors("red", "green", "blue");
+  // scene.add(axHelp);
+
+  const orbit_controls = new OrbitControls(camera, canvas);
+  // orbit_controls.enabled = false
+  orbit_controls.enableDamping = true;
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+  });
+
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // more than 2 is unnecessary
+
+  // ------ SHADOW MAPS ------
+  // ---------------------------------------------------
+  // ---------------------------------------------------
+  // ---------------------------------------------------
+  // renderer.shadowMap.enabled = true;
+  renderer.shadowMap.enabled = false;
+
+  // shadow algorythms
+  //  BasicShadowMap  ---   perfoant but lousy quality
+  //  PCFShadowMap  ---smoother but less perfomant (default)
+  //  PCFSoftShadowMap  --- less perfomant but softer edges
+  //  VSMShadowMap  --- less perfomant, more constraints, can have unexpected results
+  // renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  // renderer.shadowMap.type = THREE.VSMShadowMap;
+  // renderer.shadowMap.type = THREE.BasicShadowMap;
+
+  // ---------------------------------------------------
+  // ---------------------------------------------------
+  // ---------------------------------------------------
+  // ---------------------------------------------------
+
+  renderer.setSize(sizes.width, sizes.height);
+
+  renderer.render(scene, camera);
+
+  // -------------------------------------------------
+  // -------------------------------------------------
+  // -------------------------------------------------
+  // -------------------------------------------------
+  // -------------------------------------------------
+  // -------------------------------------------------
+  // toggle debug ui on key `h`
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "h") {
+      gui.show(gui._hidden);
+    }
+  });
+
+  // ------------- Animation loop ------------------
+  const clock = new THREE.Clock();
+  const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    // for dumping to work
+    orbit_controls.update();
+
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+  };
+
+  tick();
+
+  // ------------------------------------------------------
+  // --------------- handle resize ------------------------
+  window.addEventListener("resize", (e) => {
+    console.log("resizing");
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
+
+  // ------------------------------------------------------
+  // ----------------- enter fulll screen with double click
+
+  window.addEventListener("dblclick", () => {
+    console.log("double click");
+
+    // handling safari
+    const fullscreenElement =
+      // @ts-ignore webkit
+      document.fullscreenElement || document.webkitFullScreenElement;
+    //
+
+    // if (!document.fullscreenElement) {
+    if (!fullscreenElement) {
+      if (canvas.requestFullscreen) {
+        // go fullscreen
+        canvas.requestFullscreen();
+
+        // @ts-ignore webkit
+      } else if (canvas.webkitRequestFullScreen) {
+        // @ts-ignore webkit
+        canvas.webkitRequestFullScreen();
+      }
+    } else {
+      // @ts-ignore
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+
+        // @ts-ignore webkit
+      } else if (document.webkitExitFullscreen) {
+        // @ts-ignore webkit
+        document.webkitExitFullscreen();
+      }
+    }
+  });
+}
