69e67a976be9115ac578b3c5ce93382d7a5bf506
Rade58 <bajic.rade2@gmail.com>
2024-07-21 15:04:41 +0200

    optimizing shadows

--- src/haunted_house.ts
+++ src/haunted_house.ts
@@ -314,6 +314,9 @@ if (canvas) {
   doorLight.position.set(0, 2.2, 2.7);
 
   doorLight.castShadow = true;
+  doorLight.shadow.mapSize.width = 256;
+  doorLight.shadow.mapSize.height = 256;
+  doorLight.shadow.camera.far = 7;
 
   house.add(doorLight);
 
@@ -326,8 +329,19 @@ if (canvas) {
   const ghost3 = new THREE.PointLight("#ffff00", 2, 3);
 
   ghost1.castShadow = true;
+  ghost1.shadow.mapSize.width = 256;
+  ghost1.shadow.mapSize.height = 256;
+  ghost1.shadow.camera.far = 7;
+
   ghost2.castShadow = true;
+  ghost2.shadow.mapSize.width = 256;
+  ghost2.shadow.mapSize.height = 256;
+  ghost2.shadow.camera.far = 7;
+
   ghost3.castShadow = true;
+  ghost3.shadow.mapSize.width = 256;
+  ghost3.shadow.mapSize.height = 256;
+  ghost3.shadow.camera.far = 7;
 
   scene.add(ghost1, ghost2, ghost3);
   // ---------------------------------------------------------------------
